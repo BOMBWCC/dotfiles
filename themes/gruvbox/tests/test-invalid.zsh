c2329f0e7f4f727d4e28162a951eeb66583ceb3e
BOMBWCC <BOMBWCC@gmail.com>
2026-07-29 15:12:56 +0800

    Remove Gruvbox theme samples

--- themes/gruvbox/tests/test-invalid.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-# This file is intentionally invalid. View it; do not execute it.
-
-echo "valid command before errors"
-echo --bad-option
-)
-if [[ -d /tmp ]; then
-  echo "missing fi"
